bfacb3dd2f466b0719a9798eeb40bd13afe3d2d8
Mohammed Shifreen <mshifreen@gmail.com>
2023-08-10 09:06:01 +0530

    Temporarly commiting the current changes.

--- src/Theme.php
+++ src/Theme.php
@@ -1,20 +1,54 @@
 <?php
-/**
+/*
  * The file is part of the "webifycms/theme-green", WebifyCMS theme package.
  *
  * @see https://webifycms.com/theme/green
  *
- * @license Copyright (c) 2023 WebifyCMS
+ * @copyright Copyright (c) 2023 WebifyCMS
  * @license https://webifycms.com/theme/green/license
  * @author Mohammed Shifreen <mshifreen@gmail.com>
  */
 declare(strict_types=1);
 
-namespace Green;
+namespace Webify\Green;
 
-use Webify\Base\Infrastructure\Service\Theme\Theme as BaseTheme;
+// use Webify\Base\Infrastructure\Service\Theme\Theme as BaseTheme;
 
-final class Theme extends BaseTheme
+use Webify\Base\Domain\Service\Theme\ThemeInterface;
+use yii\base\Theme as BaseTheme;
+
+use function Webify\Base\Infrastructure\app;
+use function Webify\Base\Infrastructure\url;
+
+/**
+ * WebifyCMS Green theme main class.
+ */
+final class Theme extends BaseTheme implements ThemeInterface
 {
+	public function init(): void
+	{
+		$this->setBasePath(\dirname(__DIR__));
+		$this->setBaseUrl($this->publishAssets());
 
+		$this->pathMap = [
+			'@App/templates'  => $this->getBasePath() . '/templates',
+		];
+
+		parent::init();
+	}
+
+	public function getId(): string
+	{
+		return 'green';
+	}
+
+	/**
+	 * Published assets of this theme to the public folder.
+	 */
+	private function publishAssets(): string
+	{
+		$published = app()->assetManager->publish(\dirname(__DIR__) . '/dist');
+
+		return url($published[1]);
+	}
 }
