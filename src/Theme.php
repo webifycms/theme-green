06625a45fb5d0fc47060c04fd1b0f2424c90f0cb
Mohammed Shifreen <mshifreen@gmail.com>
2024-12-31 23:46:30 +0530

    * Made improvements on assets management. * Dependencies upgraded. * Did some other tweaks and fixes.

--- src/Theme.php
+++ src/Theme.php
@@ -1,4 +1,5 @@
 <?php
+
 /**
  * The file is part of the "webifycms/theme-green", WebifyCMS theme package.
  *
@@ -15,20 +16,19 @@ namespace Webify\Green;
 use Webify\Base\Domain\Service\Theme\ThemeInterface;
 use Webify\Base\Infrastructure\Service\Theme\Theme as BaseTheme;
 
-use function Webify\Base\Infrastructure\app;
-
 /**
  * WebifyCMS Green theme main class.
  */
 final class Theme extends BaseTheme implements ThemeInterface
 {
+	public const THEME_ID = 'green';
+
 	public function init(): void
 	{
 		$this->setBasePath(\dirname(__DIR__));
-		$this->setBaseUrl($this->publishAssets());
 
-		$this->pathMap = [
-			'@App/templates'  => $this->getBasePath() . '/templates',
+		$this->pathMap    = [
+			'@App/templates' => $this->getBasePath() . '/templates',
 		];
 
 		parent::init();
@@ -36,13 +36,6 @@ final class Theme extends BaseTheme implements ThemeInterface
 
 	public function getId(): string
 	{
-		return 'green';
-	}
-
-	private function publishAssets(): string
-	{
-		$published = app()->assetManager->publish(\dirname(__DIR__) . '/dist');
-
-		return $published[1];
+		return self::THEME_ID;
 	}
 }
---
+++ src/ViteHelper.php
@@ -0,0 +1,93 @@
+<?php
+
+/**
+ * The file is part of the "webifycms/theme-green", WebifyCMS theme package.
+ *
+ * @see https://webifycms.com/theme/green
+ *
+ * @copyright Copyright (c) 2023 WebifyCMS
+ * @license https://webifycms.com/theme/green/license
+ * @author Mohammed Shifreen <mshifreen@gmail.com>
+ */
+declare(strict_types=1);
+
+namespace Webify\Green;
+
+use Webify\Base\Domain\Exception\TranslatableRuntimeException;
+
+use function Webify\Base\Infrastructure\get_alias;
+
+/**
+ * Provides utility methods for working with Vite in a PHP application.
+ * This class supports both development mode (with a running Vite dev server)
+ * and production mode (with a generated manifest.json file).
+ */
+final class ViteHelper
+{
+	/**
+	 * Path to the distribution directory for theme green assets.
+	 */
+	public string $distPath = '@Theme/dist';
+
+	/**
+	 * Specifies the file path to the main JavaScript entry point for the application.
+	 * It should match with Vite configuration.
+	 */
+	public string $entryPoint = 'assets/js/main.js';
+
+	/**
+	 * Specifies the file location of the Vite manifest file.
+	 */
+	private string $manifestFile = 'manifest.json';
+
+	/**
+	 * The URL for the development server.
+	 *
+	 * TODO: Should handle through the env variable.
+	 */
+	private string $devServerUrl = 'http://host.docker.internal:5000/';
+
+	/**
+	 * Checks if the development server is currently running.
+	 *
+	 * @return bool returns true if the development server is running, otherwise false
+	 */
+	public function isDevServerRunning(): bool
+	{
+		return !empty(file_get_contents($this->devServerUrl . '@vite/client'));
+	}
+
+	/**
+	 * Retrieves the manifest file and extracts the configuration for the specified entry point.
+	 *
+	 * @return array<string, mixed> the configuration for the specified entry point within the Vite manifest file
+	 *
+	 * @throws TranslatableRuntimeException if the manifest file is not found, cannot be read,
+	 *                                      or the entry point is missing
+	 */
+	public function getManifest(): array
+	{
+		$manifestFilePath = get_alias($this->distPath . '/' . $this->manifestFile);
+
+		if (null === $manifestFilePath || !file_exists($manifestFilePath)) {
+			throw new TranslatableRuntimeException('green.unable_to_find_vite_manifest_file');
+		}
+
+		$content = file_get_contents($manifestFilePath);
+
+		if (!$content) {
+			throw new TranslatableRuntimeException('green.unable_to_read_vite_manifest_file');
+		}
+
+		$manifest = json_decode($content, true);
+
+		if (!isset($manifest[$this->entryPoint])) {
+			throw new TranslatableRuntimeException(
+				'green.entry_point_not_found',
+				['entryPoint' => $this->entryPoint]
+			);
+		}
+
+		return $manifest[$this->entryPoint];
+	}
+}
